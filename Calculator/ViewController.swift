0dc0a002298a09d98716d1d86267a9f14de5d326
peterszabo <szabopeterakos@gmail.com>
2019-01-04 22:06:50 +0100

    operators enum, negate func

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: PROPERTIES
+    enum OPERATOR: String{
+        case addition = "+" // 14
+        case substraction = "-" // 15
+        case divide = "/" // 12
+        case multiple = "*" // 13
+        case equals = "=" // 16
+    }
+    
     @IBOutlet weak var numMonitor: UILabel!
     
+    var currentOperator: OPERATOR!
+    
+    @IBOutlet weak var testDisplayArea: UIButton!
+    
+    var isNegate: Bool = false
+    
+    //MARK: INIT
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    //MARK: LOGIC
     @IBAction func numbers(_ sender: UIButton) {
         if(numMonitor.text=="0"){
             numMonitor.text = ""
@@ -25,8 +42,41 @@ class ViewController: UIViewController {
         numMonitor.text = current
     }
     
+    @IBAction func operators(_ sender: UIButton) {
+        var currentOperator : OPERATOR!
+        
+        switch sender.tag {
+        case 12:
+            currentOperator = OPERATOR.divide
+        case 13:
+            currentOperator = OPERATOR.multiple
+        case 14:
+            currentOperator = OPERATOR.substraction
+        case 15:
+            currentOperator = OPERATOR.addition
+        case 16:
+            currentOperator = OPERATOR.equals
+        default:
+            print("something goes wrong")
+        }
+        
+        testDisplayArea.setTitle(currentOperator.rawValue, for: .normal)
+    }
+    
     @IBAction func resetButton(_ sender: UIButton) {
         numMonitor.text = "0"
+    }
+    
+    @IBAction func negationControl(_ sender: UIButton) {
+        print(isNegate)
+        isNegate = !isNegate
+        if isNegate {
+            numMonitor.text = "-" + numMonitor.text!
+        } else {
+            if (numMonitor.text?.hasPrefix("-"))!{
+                numMonitor.text = String(numMonitor.text!.dropFirst())
+            }
+        }
     }
     
 }
