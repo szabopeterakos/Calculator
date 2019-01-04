34851a3f19e14db2fe4ddffcbd16177de2087902
peterszabo <szabopeterakos@gmail.com>
2019-01-04 12:30:03 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Szabó Péter on 2019. 01. 04..
+//  Copyright © 2019. Szabó Péter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
